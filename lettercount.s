7ccd4466cf47b6b49b277af2dbaef59f0af083a8
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2025-01-16 19:17:54 -0300

    Update lettercount.s

--- lettercount.s
+++ lettercount.s
@@ -1,1 +1,23 @@
+.globl _start
+.section .data
+mytext:
+	.ascii "This is a string of characters. \0"
 
+.section .text
+_start:
+	movq $mytext, %rbx
+	movq $0, %rdi
+
+mainloop:
+	movb (%rbx), %al
+	cmpb $0, %al
+	je finish
+	incq %rdi
+
+loopcontrol:
+	incq %rbx
+	jmp mainloop
+
+finish:
+	movq $60, %rax
+	syscall

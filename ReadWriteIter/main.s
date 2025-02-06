b103a46afcbb8119bfe1855ec2e5134665dc9781
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2025-02-06 20:29:54 -0300

    Update main.s

--- ReadWriteIter/main.s
+++ ReadWriteIter/main.s
@@ -1,43 +1,43 @@
 .globl main
 .section .data
 input:
-	.ascii "Indique um numero:\n\0"
+        .ascii "Indique um numero:\n\0"
 scan:
-	.ascii "%d\0"
+        .ascii "%d\0"
 output:
-	.ascii "%d\n\0"
+        .ascii "%d\n\0"
 
 .section .text
 
-	.equ NUM_P, -8
+        .equ NUM_P, -8
 
 main:
-	push %rbp
-	movq %rsp, %rbp
-	subq $16, %rsp
+        push %rbp
+        movq %rsp, %rbp
+        subq $16, %rsp
 
-	movq stdout, %rdi
-	movq $input, %rsi
-	movq $0, %rax
-	call fprintf
+        movq stdout, %rdi
+        movq $input, %rsi
+        movq $0, %rax
+        call fprintf
 
-	movq stdin, %rdi
-	movq $scan, %rsi
-	leaq NUM_P(%rbp), %rdx
-	movq $0, %rax
-	call fscanf
+        movq stdin, %rdi
+        movq $scan, %rsi
+        leaq NUM_P(%rbp), %rdx
+        movq $0, %rax
+        call fscanf
 
-	movq NUM_P(%rbp), %rax
-	call mainfunc
-	movq %rax, NUM_P(%rbp)
+        movq %rsi, %rax
+        call mainfunc
+        movq %rax, NUM_P(%rbp)
 
-	movq stdout, %rdi
-	movq $output, %rsi
-	movq NUM_P(%rbp), %rdx
-	movq $0, %rax
-	call fprintf
+        movq stdout, %rdi
+        movq $output, %rsi
+        movq NUM_P(%rbp), %rdx
+        movq $0, %rax
+        call fprintf
 
-	call iter
+        call iter
 
-	leave
-	ret
+        leave
+        ret
